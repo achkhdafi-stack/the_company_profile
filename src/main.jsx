a5e47559750a439f54d04a9faf7e5cd7b62f916a
achkhdafi-stack <ach.khdafi@gmail.com>
2026-07-30 19:03:02 +0700

    commit again

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
+import App from "./App.jsx";
 import { LanguageProvider } from "./context/LanguageContext";
 import { AuthProvider } from "./context/AuthContext";
 
 import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <LanguageProvider>
-    <AuthProvider>
-      <StrictMode>
-        <BrowserRouter>
+  <StrictMode>
+    <HashRouter>
+      <LanguageProvider>
+        <AuthProvider>
           <App />
-        </BrowserRouter>
-      </StrictMode>
-    </AuthProvider>
-  </LanguageProvider>
+        </AuthProvider>
+      </LanguageProvider>
+    </HashRouter>
+  </StrictMode>
 );
